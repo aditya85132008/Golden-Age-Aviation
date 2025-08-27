576f9f42fefa26dbf487210a22bd1f6e332eca35
aditya85132008 <adityagnps1993@gmail.com>
2025-08-27 21:10:48 +0530

    Add files via upload

---
+++ webserver.py
@@ -0,0 +1,14 @@
+from flash import Flask
+from threading import Thread
+
+app = Flash('')
+@app.route('/')
+def home():
+    return "Discord bot ok"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    t=Thread(target=run)
+    t.start()
